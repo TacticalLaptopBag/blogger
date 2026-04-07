443da76bce0dabe634448adf819557e11dc6dbff
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-04-07 14:39:17 -0400

    Create blog_post table

---
+++ migrations/2026-04-07-174641-0000_blog_post/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE blog_post (
+    id TEXT PRIMARY KEY NOT NULL UNIQUE,
+    title TEXT NOT NULL,
+    author_id TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT(datetime('now')),
+    modified_at TEXT DEFAULT(NULL),
+    post_content TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users (id)
+);
