a9a2b321a46c3ca646dac8cb7bb0fd14c6b2b623
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-04-07 14:39:06 -0400

    Enforce unique user ids

--- migrations/2026-04-07-023722-0000_create_users/up.sql
+++ migrations/2026-04-07-023722-0000_create_users/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE users (
-    id TEXT PRIMARY KEY NOT NULL,
+    id TEXT PRIMARY KEY NOT NULL UNIQUE,
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     created_at TEXT NOT NULL DEFAULT(datetime('now'))
